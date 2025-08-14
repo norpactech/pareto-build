13b1ec6865a57dbacc348b64f47a83b1ce2e2572
Scott Klakken <scott@northernpacific.tech>
2025-08-14 08:58:01 -0700

    Standard Environment Variables

--- src/main/java/com/norpactech/pf/build/Application.java
+++ src/main/java/com/norpactech/pf/build/Application.java
@@ -17,21 +17,21 @@ public class Application {
 
   public static void main(String[] args) throws Exception {
   
-    String username   = System.getenv("PARETO_BUILD_USERNAME");
-    String password   = System.getenv("PARETO_BUILD_PASSWORD");
+    String username   = System.getenv("PARETO_USERNAME");
+    String password   = System.getenv("PARETO_PASSWORD");
     String buildFile  = System.getenv("PARETO_BUILD_FILE");
-    String factoryURL = System.getenv("PARETO_BUILD_FACTORY_URL");
-    String tokenURL   = System.getenv("PARETO_BUILD_TOKEN_URL");
+    String factoryURL = System.getenv("PARETO_FACTORY_URL");
+    String tokenURL   = System.getenv("PARETO_TOKEN_URL");
 
     logger.info("Beginning Pareto Build");
 
     if (StringUtils.isEmpty(username)) {
-      logger.error("Null or empty username. Set environment variable: PARETO_BUILD_USERNAME. Terminating...");
+      logger.error("Null or empty username. Set environment variable: PARETO_USERNAME. Terminating...");
       System.exit(1);
     }
     
     if (StringUtils.isEmpty(password)) {
-      logger.error("Null or empty password. Set environment variable: PARETO_BUILD_PASSWORD. Terminating...");
+      logger.error("Null or empty password. Set environment variable: PARETO_PASSWORD. Terminating...");
       System.exit(1);
     }
     
@@ -41,12 +41,12 @@ public class Application {
     }
 
     if (StringUtils.isEmpty(factoryURL)) {
-      logger.error("Null or empty Pareto Factory URL. Set environment variable: PARETO_BUILD_FACTORY_URL. Terminating...");
+      logger.error("Null or empty Pareto Factory URL. Set environment variable: PARETO_FACTORY_URL. Terminating...");
       System.exit(1);
     }
 
     if (StringUtils.isEmpty(tokenURL)) {
-      logger.error("Null or empty Access Token URL. Set environment variable: PARETO_BUILD_TOKEN_URL. Terminating...");
+      logger.error("Null or empty Access Token URL. Set environment variable: PARETO_TOKEN_URL. Terminating...");
       System.exit(1);
     }    
     logger.info("Pareto Build using file: {}", buildFile);
